c71d29ba0fee945d28f559d6840a871d94e7cdcc
Bryan Alvarado <bryan.alvarado@usm.cl>
2025-12-10 22:17:50 -0300

    Reorganization of the code

--- src/GCN.py
+++ src/GCN.py
@@ -1,12 +1,38 @@
-import os
-
 import torch
+import torch.nn as nn
 import torch_geometric
-import gzip
-import pickle
-import numpy as np
-import time
+import pickle as pkl
+from constants import LITERALS_GRAPH, VARIABLES_GRAPH, GCN_LAYER, GTR_LAYER
 
+def flip(x, dim=0):
+    # Move the specified dimension to the front
+    x = x.transpose(0, dim)
+    
+    # Get the shape of the tensor
+    shape = x.shape
+    
+    # Ensure the leading dimension is even
+    if shape[0] % 2 != 0:
+        raise ValueError("The size of the specified dimension must be even.")
+    
+    # Reshape to collapse the leading dimension to pairs
+    new_shape = (shape[0] // 2, 2) + shape[1:]
+    x = x.view(new_shape)
+    
+    # Initialize an empty tensor with the same shape as the reshaped tensor
+    result = torch.empty_like(x)
+    
+    # Perform the swapping for related pairs
+    result[:, 0] = x[:, 1]
+    result[:, 1] = x[:, 0]
+    
+    # Reshape back to the original shape
+    result = result.view(shape)
+    
+    # Move the dimensions back to their original order
+    result = result.transpose(0, dim)
+    
+    return result
 def inverse_join_literals(tensor):
     n, two_k = tensor.shape
     if two_k % 2 != 0:
@@ -42,14 +68,16 @@ def join_literals(tensor):
     tensor = tensor.reshape(n, 2 * k)
     
     return tensor
-class BackboneAndValuesPredictor(torch.nn.Module):
-    def __init__(self):
+    
+class BackbonePredictor(torch.nn.Module):
+    def __init__(self,graph_type:str, layer_type:str, num_layers:int, use_literals_message:bool, emb_size:int=64):
         super().__init__()
-        emb_size = 64
         cons_nfeats = 4
         edge_nfeats = 1
         var_nfeats = 6
-
+        self.num_layers = num_layers
+        self.graph_type = graph_type
+        
         # CONSTRAINT EMBEDDING
         self.cons_embedding = torch.nn.Sequential(
             torch.nn.LayerNorm(cons_nfeats),
@@ -72,158 +100,81 @@ class BackboneAndValuesPredictor(torch.nn.Module):
             torch.nn.Linear(emb_size, emb_size),
             torch.nn.ReLU(),
         )
+
+        if layer_type == GCN_LAYER:
+            self.conv_v_to_c = torch.nn.ModuleList([
+                BipartiteGraphConvolution(emb_size) for _ in range(num_layers)
+            ])
+            self.conv_c_to_v = torch.nn.ModuleList([
+                BipartiteGraphConvolution(emb_size) for _ in range(num_layers)
+            ])
+        elif layer_type == GTR_LAYER:
+            self.conv_v_to_c = torch.nn.ModuleList([
+                BipartiteTransformerLayer(emb_size) for _ in range(num_layers)
+            ])
+            self.conv_c_to_v = torch.nn.ModuleList([
+                BipartiteTransformerLayer(emb_size) for _ in range(num_layers)
+            ])
+        else:
+            raise ValueError(f"Unknown layer_type requested. Choose '{GCN_LAYER}' or '{GTR_LAYER}'.")
         
-        self.conv_v_to_c = BipartiteGraphConvolution()
-        self.conv_c_to_v = BipartiteGraphConvolution()
+        if graph_type == LITERALS_GRAPH:
+            output_emb_size = 2 * emb_size
+        elif graph_type == VARIABLES_GRAPH:
+            output_emb_size = emb_size
+        else:
+            raise ValueError(f"Unknown graph_type requested: {graph_type}. Choose '{LITERALS_GRAPH}' or '{VARIABLES_GRAPH}'.")
+        
+        if use_literals_message:
+            assert graph_type == LITERALS_GRAPH, "Literals message passing is only supported for literals graph type."
+            self.l_to_l = torch.nn.ModuleList([
+                LiteralsMessage(emb_size) for _ in range(num_layers)
+            ])
 
-        self.l_to_l = LiteralsMessage(emb_size)
-
-        self.conv_v_to_c2 = BipartiteGraphConvolution()
-        self.conv_c_to_v2 = BipartiteGraphConvolution()
-
-        self.l_to_l2 = LiteralsMessage(emb_size)
-
-        self.output_module = torch.nn.Sequential(
-            torch.nn.Linear(2*emb_size, 2*emb_size),
-            torch.nn.ReLU(),
-            torch.nn.Linear(2*emb_size, 3, bias=False),
+        self.output_module = nn.Sequential(
+            nn.Linear(output_emb_size, output_emb_size),
+            nn.ReLU(),
+            nn.Linear(output_emb_size, 3, bias=False),
         )
+
 
     def forward(
         self, constraint_features, edge_indices, edge_features, variable_features
     ):
         reversed_edge_indices = torch.stack([edge_indices[1], edge_indices[0]], dim=0)
 
-        # First step: linear embedding layers to a common dimension (64)
+        # First step: linear embedding layers to a common dimension
         constraint_features = self.cons_embedding(constraint_features)
         edge_features = self.edge_embedding(edge_features)
         variable_features = self.var_embedding(variable_features)
 
-        # Two half convolutions
-        constraint_features = self.conv_v_to_c(variable_features, reversed_edge_indices, edge_features, constraint_features)
-        variable_features = self.conv_c_to_v(constraint_features, edge_indices, edge_features, variable_features)
+        # Iterate over the gnn layers
+        for i in range(self.num_layers):
+            # Apply the gnn layer from variable to constraint
+            constraint_features = self.conv_v_to_c[i](variable_features, reversed_edge_indices, edge_features, constraint_features)
+            # Apply the gnn layer from constraint to variable
+            variable_features = self.conv_c_to_v[i](constraint_features, edge_indices, edge_features, variable_features)
+            # Apply the literals message passing
+            if hasattr(self, 'l_to_l'):
+                # If using literals message passing, apply it
+                variable_features = self.l_to_l[i](variable_features)
 
-        variable_features = self.l_to_l(variable_features)
+        if self.graph_type == LITERALS_GRAPH:
+            # If the graph type is literals, we need to join the literals
+            variable_features = join_literals(variable_features)
 
-        constraint_features = self.conv_v_to_c2(variable_features, reversed_edge_indices, edge_features, constraint_features)
-        variable_features = self.conv_c_to_v2(constraint_features, edge_indices, edge_features, variable_features)
-
-        variable_features = self.l_to_l2(variable_features)
-        variable_features = join_literals(variable_features)
-        # A final MLP on the variable features
-        output = self.output_module(variable_features).squeeze(-1)
-        return output
+        return self.output_module(variable_features)
     
-class MilpBackboneAndValuesPredictor(torch.nn.Module):
-    def __init__(self):
-        super().__init__()
-        emb_size = 64
-        cons_nfeats = 4
-        edge_nfeats = 1
-        var_nfeats = 6
-
-        # CONSTRAINT EMBEDDING
-        self.cons_embedding = torch.nn.Sequential(
-            torch.nn.LayerNorm(cons_nfeats),
-            torch.nn.Linear(cons_nfeats, emb_size),
-            torch.nn.ReLU(),
-            torch.nn.Linear(emb_size, emb_size),
-            torch.nn.ReLU(),
-        )
-
-        # EDGE EMBEDDING
-        self.edge_embedding = torch.nn.Sequential(
-            torch.nn.LayerNorm(edge_nfeats),
-        )
-
-        # VARIABLE EMBEDDING
-        self.var_embedding = torch.nn.Sequential(
-            torch.nn.LayerNorm(var_nfeats),
-            torch.nn.Linear(var_nfeats, emb_size),
-            torch.nn.ReLU(),
-            torch.nn.Linear(emb_size, emb_size),
-            torch.nn.ReLU(),
-        )
-        
-        self.conv_v_to_c = BipartiteGraphConvolution()
-        self.conv_c_to_v = BipartiteGraphConvolution()
-
-        self.conv_v_to_c2 = BipartiteGraphConvolution()
-        self.conv_c_to_v2 = BipartiteGraphConvolution()
-
-        self.output_module = torch.nn.Sequential(
-            torch.nn.Linear(emb_size, emb_size),
-            torch.nn.ReLU(),
-            torch.nn.Linear(emb_size, 3, bias=False),
-        )
-
-    def forward(
-        self, constraint_features, edge_indices, edge_features, variable_features
-    ):
-        reversed_edge_indices = torch.stack([edge_indices[1], edge_indices[0]], dim=0)
-
-        # First step: linear embedding layers to a common dimension (64)
-        constraint_features = self.cons_embedding(constraint_features)
-        edge_features = self.edge_embedding(edge_features)
-        variable_features = self.var_embedding(variable_features)
-
-        # Two half convolutions
-        constraint_features = self.conv_v_to_c(variable_features, reversed_edge_indices, edge_features, constraint_features)
-        variable_features = self.conv_c_to_v(constraint_features, edge_indices, edge_features, variable_features)
-
-        constraint_features = self.conv_v_to_c2(variable_features, reversed_edge_indices, edge_features, constraint_features)
-        variable_features = self.conv_c_to_v2(constraint_features, edge_indices, edge_features, variable_features)
-
-        # A final MLP on the variable features
-        output = self.output_module(variable_features).squeeze(-1)
-        return output
-    
-def flip(x, dim=0):
-    # Move the specified dimension to the front
-    x = x.transpose(0, dim)
-    
-    # Get the shape of the tensor
-    shape = x.shape
-    
-    # Ensure the leading dimension is even
-    if shape[0] % 2 != 0:
-        raise ValueError("The size of the specified dimension must be even.")
-    
-    # Reshape to collapse the leading dimension to pairs
-    new_shape = (shape[0] // 2, 2) + shape[1:]
-    x = x.view(new_shape)
-    
-    # Initialize an empty tensor with the same shape as the reshaped tensor
-    result = torch.empty_like(x)
-    
-    # Perform the swapping for related pairs
-    result[:, 0] = x[:, 1]
-    result[:, 1] = x[:, 0]
-    
-    # Reshape back to the original shape
-    result = result.view(shape)
-    
-    # Move the dimensions back to their original order
-    result = result.transpose(0, dim)
-    
-    return result
+# Message Passing Layers (LiteralsMessage, BipartiteGraphConvolution, BipartiteTransformerLayer)
 class LiteralsMessage(torch.nn.Module):
     def __init__(self,size):
         super().__init__()
         self.a = torch.nn.Parameter(torch.randn(size))
         self.b = torch.nn.Parameter(torch.randn(size))
-        self.relu = torch.nn.ReLU()
+        self.activation = torch.nn.ReLU()
     def forward(self,literals):
-        return self.relu(self.a*literals + self.b*flip(literals))
+        return self.activation(self.a*literals + self.b*flip(literals))
     
-class LiteralsMessageFull(torch.nn.Module):
-    def __init__(self,size):
-        super().__init__()
-        self.output = torch.nn.Sequential(
-            torch.nn.Linear(2*size, 2*size),
-            torch.nn.ReLU())
-    def forward(self,literals):
-        return inverse_join_literals(self.output(join_literals(literals)))
 class BipartiteGraphConvolution(torch_geometric.nn.MessagePassing):
     """
     The bipartite graph convolution is already provided by pytorch geometric and we merely need
@@ -270,8 +221,6 @@ class BipartiteGraphConvolution(torch_geometric.nn.MessagePassing):
             node_features=(left_features, right_features),
             edge_features=edge_features,
         )
-        #b=torch.cat([self.post_conv_module(output), right_features], dim=-1)
-        #a=self.output_module(torch.cat([self.post_conv_module(output), right_features], dim=-1))
 
         return self.output_module(
             torch.cat([self.post_conv_module(output), right_features], dim=-1)
@@ -279,12 +228,6 @@ class BipartiteGraphConvolution(torch_geometric.nn.MessagePassing):
 
 
     def message(self, node_features_i, node_features_j, edge_features):
-        #node_features_i,the node to be aggregated
-        #node_features_j,the neighbors of the node i
-
-        # print("node_features_i:",node_features_i.shape)
-        # print("node_features_j",node_features_j.shape)
-        # print("edge_features:",edge_features.shape)
 
         output = self.feature_module_final(
             self.feature_module_left(node_features_i)
@@ -293,35 +236,64 @@ class BipartiteGraphConvolution(torch_geometric.nn.MessagePassing):
         )
 
         return output
+    
+class BipartiteTransformerLayer(torch_geometric.nn.MessagePassing):
+    def __init__(self, emb_size, heads=4):
+        super().__init__(aggr='add')
+        self.heads = heads
+        self.emb_size = emb_size
+        self.head_dim = emb_size // heads
 
-class GraphDatasetTriOutput(torch_geometric.data.Dataset):
-    """
-    This class encodes a collection of graphs, as well as a method to load such graphs from the disk.
-    It can be used in turn by the data loaders provided by pytorch geometric.
-    """
+        self.q_lin = nn.Linear(emb_size, emb_size)
+        self.k_lin = nn.Linear(emb_size, emb_size)
+        self.v_lin = nn.Linear(emb_size, emb_size)
+        self.edge_lin = nn.Linear(1, emb_size)
 
-    def __init__(self, sample_files: list):
+        self.out_lin = nn.Linear(emb_size, emb_size)
+        self.norm = nn.LayerNorm(emb_size)
+
+    def forward(self, x_src, edge_index, edge_attr, x_tgt):
+        out = self.propagate(edge_index, x=(x_src, x_tgt), edge_attr=edge_attr)
+        return self.norm(x_tgt + self.out_lin(out))  # residual
+
+    def message(self, x_i, x_j, edge_attr, index):
+        # Attention over neighbors j -> i
+        Q = self.q_lin(x_i).view(-1, self.heads, self.head_dim)
+        K = self.k_lin(x_j).view(-1, self.heads, self.head_dim)
+        V = self.v_lin(x_j).view(-1, self.heads, self.head_dim)
+        E = self.edge_lin(edge_attr).view(-1, self.heads, self.head_dim)
+
+        scores = (Q * (K + E)).sum(-1) / self.head_dim**0.5  # [E, H]
+        alpha = torch_geometric.utils.softmax(scores, index)
+
+        return (V * alpha.unsqueeze(-1)).view(-1, self.emb_size)
+# GraphDataset
+class GraphBackboneDataset(torch_geometric.data.Dataset):
+
+    def __init__(self, sample_files: list, graph_type: str):
         super().__init__(root=None, transform=None, pre_transform=None)
         self.sample_files = sample_files
+        self.graph_type = graph_type
+        assert graph_type in [LITERALS_GRAPH, VARIABLES_GRAPH], f"Unknown graph type requested: {graph_type}. Choose '{LITERALS_GRAPH}' or '{VARIABLES_GRAPH}'."
 
     def len(self):
         return len(self.sample_files)
 
-
     def process_sample(self,filepath):
         with open(filepath, "rb") as f:
-            data = pickle.load(f)
-        BG = data['X']
-        sols = data['y']
-        return BG,sols
+            data = pkl.load(f)
+        bipartite_graph =  data['bipartite_graph']
+        target = data['target']
+        assert data["graph_type"] == self.graph_type, f"Graph type mismatch: expected {self.graph_type}, got {data['graph_type']}"
+        return bipartite_graph, target
 
     def get_graph_components(A, v_nodes, c_nodes):
         constraint_features = c_nodes
         edge_indices = A._indices()
 
         variable_features = v_nodes
-        edge_features =A._values().unsqueeze(1)
-        edge_features=torch.ones(edge_features.shape)
+        edge_features = A._values().unsqueeze(1)
+        #edge_features = torch.ones(edge_features.shape)
         constraint_features = torch.nan_to_num(constraint_features,1)
         
         return constraint_features, edge_indices, edge_features, variable_features
@@ -331,30 +303,26 @@ class GraphDatasetTriOutput(torch_geometric.data.Dataset):
         This method loads a node bipartite graph observation as saved on the disk during data collection.
         """
 
-        # nbp, sols, objs, varInds, varNames = self.process_sample(self.sample_files[index])
-        BG, sols = self.process_sample(self.sample_files[index])
-        A, v_map, v_nodes, c_nodes = BG
-        constraint_features, edge_indices, edge_features, variable_features = GraphDatasetTriOutput.get_graph_components(A, v_nodes, c_nodes)    
+        bipartite_graph, target = self.process_sample(self.sample_files[index])
+        A, v_map, v_nodes, c_nodes = bipartite_graph
+        constraint_features, edge_indices, edge_features, variable_features = GraphBackboneDataset.get_graph_components(A, v_nodes, c_nodes)    
 
         graph = BipartiteNodeData(
-            torch.FloatTensor(constraint_features.cpu()),
-            torch.LongTensor(edge_indices.cpu()),
-            torch.FloatTensor(edge_features.cpu()),
-            torch.FloatTensor(variable_features.cpu()),
+            constraint_features.cpu().float(),
+            edge_indices.cpu().long(),
+            edge_features.cpu().float(),
+            variable_features.cpu().float(),
         )
-        
         
         
         # We must tell pytorch geometric how many nodes there are, for indexing purposes
         graph.num_nodes = constraint_features.shape[0] + variable_features.shape[0]
-        graph.y = torch.FloatTensor(sols)
-        l1 = graph.y[::2]>.5
-        l0 = graph.y[1::2]>.5
-        is_not_backbone = ~(l1 | l0)
-        #concatenate l0,l1,is_not_backbone
-        graph.y = l1.long()+ is_not_backbone.long() * 2
-        graph.ntvars = variable_features.shape[0]
-        graph.varNames = [v for _,v in sorted(v_map.items(), key=lambda item: item[1])]
+        graph.target = target
+        if self.graph_type == LITERALS_GRAPH:
+            graph.num_original_bin_variables = variable_features.shape[0] // 2
+        else:
+            graph.num_original_bin_variables = variable_features.shape[0]
+        #graph.varNames = [v for _,v in sorted(v_map.items(), key=lambda item: item[1])]
 
         return graph
 
@@ -389,8 +357,7 @@ class BipartiteNodeData(torch_geometric.data.Data):
             return torch.tensor(
                 [[self.constraint_features.size(0)], [self.variable_features.size(0)]]
             )
-        elif key == "candidates":
+        elif key == "candidates": # This is not used anymore
             return self.variable_features.size(0)
         else:
             return super().__inc__(key, value, *args, **kwargs)
-
--- src/get_bipartite_graph.py
+++ src/get_bipartite_graph.py
@@ -1,5 +1,6 @@
 import pyscipopt as scp
 import torch
+from constants import VARIABLES_GRAPH, LITERALS_GRAPH
 def normalize_model(model,add_complement_constraints=False):
     """
     Normalize the SCIP model such that all coefficients in constraints are non-negative.
@@ -86,7 +87,7 @@ def normalize_features(features : str):
     #se normaliza para que las features esten entre 0 y 1
     return torch.clamp((features - mins)/ diff, 1e-5, 1)#se limita para que esten entre 1e-5 y 1
 
-def get_bipartite_graph(ins_name : str):    
+def get_bipartite_graph_literals(ins_name : str):    
     m = scp.Model()
     m.hideOutput(True)
     m.readProblem(ins_name)
@@ -139,7 +140,7 @@ def get_bipartite_graph(ins_name : str):
     # [2] rhs: right-hand-side value of the constraint
     # [3] sense: the sense of the constraint
     indices_spr = [[], []] # Coordenadas de arcos [constraintIndexes, variableIndexes]
-    values_spr = [] # Peso que habra en la matriz de adjacencia (siempre seran 1)
+    values_spr = [] # Peso que habra en la matriz de adjacencia
 
     for cind, c in enumerate(cons): #itera sobre las constraints
         coeff = m.getValsLinear(c) #diccionario de coeficientes {nombreVariable: coeficiente}
@@ -161,7 +162,7 @@ def get_bipartite_graph(ins_name : str):
                 l_indx = l_map[lit] #obtiene el indice del literal
                 indices_spr[0].append(cind) #indice de la constraint
                 indices_spr[1].append(l_indx) #indice de la variable
-                values_spr.append(1) 
+                values_spr.append(coeff[lit]) #valor del coeficiente (peso del arco)
             
                 l_nodes[l_indx][1] += coeff[lit] / ncons #Añade el coeficiente normalizado (segun la restriccion) al embedding de la variable
                 l_nodes[l_indx][2] += 1 #Agrega 1 al contador de restricciones en la que participa el literal
@@ -191,7 +192,7 @@ def get_bipartite_graph(ins_name : str):
                 l_indx = l_map[lit] #obtiene el indice del literal
                 indices_spr[0].append(ncons) #Indice de la restriccion (func. objetivo)
                 indices_spr[1].append(l_indx) #Indice del literal
-                values_spr.append(1)
+                values_spr.append(v)
                 l_nodes[l_indx][0] = v #añade el coeficiente de la variable en la funcion objetivo al embedding de la variable
 
                 obj_node[0] += v #acumula los coeficientes de todas las variables
@@ -208,3 +209,164 @@ def get_bipartite_graph(ins_name : str):
 
     A = torch.sparse_coo_tensor(indices_spr, values_spr, (ncons, len(mvars)*2))
     return A, l_map, l_nodes, c_nodes
+
+def get_bipartite_graph_variables(ins_name):
+    # Same function used by PaS and ConPaS. Code extracted from https://github.com/sribdcn/Predict-and-Search_MILP_method/blob/main/helper.py
+    # The only difference is that here the tensors are not moved to a specific device.
+    epsilon = 1e-6
+
+    # vars:  [obj coeff, norm_coeff, degree, Bin?]
+    m = scp.Model()
+    m.hideOutput(True)
+    m.readProblem(ins_name)
+
+    ncons = m.getNConss()
+    nvars = m.getNVars()
+
+    mvars = m.getVars()
+    mvars.sort(key=lambda v: v.name)
+
+
+    v_nodes = []
+
+    b_vars = []
+
+    ori_start = 6
+    emb_num = 15
+
+    for i in range(len(mvars)):
+        tp = [0] * ori_start
+        tp[3] = 0
+        tp[4] = 1e+20
+        # tp=[0,0,0,0,0]
+        if mvars[i].vtype() == 'BINARY':
+            tp[ori_start - 1] = 1
+            b_vars.append(i)
+
+        v_nodes.append(tp)
+    v_map = {}
+
+    for indx, v in enumerate(mvars):
+        v_map[v.name] = indx
+
+    obj = m.getObjective()
+    obj_cons = [0] * (nvars + 2)
+    indices_spr = [[], []]
+    values_spr = []
+    obj_node = [0, 0, 0, 0]
+    for e in obj:
+        vnm = e.vartuple[0].name
+        v = obj[e]
+        v_indx = v_map[vnm]
+        obj_cons[v_indx] = v
+        if v != 0:
+            indices_spr[0].append(0)
+            indices_spr[1].append(v_indx)
+            # values_spr.append(v)
+            values_spr.append(1)
+        v_nodes[v_indx][0] = v
+
+        # print(v_indx,float(nvars),v_indx/float(nvars),v_nodes[v_indx][ori_start:ori_start+emb_num])
+
+        obj_node[0] += v
+        obj_node[1] += 1
+    obj_node[0] /= obj_node[1]
+    # quit()
+
+    cons = m.getConss()
+    new_cons = []
+    for cind, c in enumerate(cons):
+        coeff = m.getValsLinear(c)
+        if len(coeff) == 0:
+            # print(coeff,c)
+            continue
+        new_cons.append(c)
+    cons = new_cons
+    ncons = len(cons)
+    cons_map = [[x, len(m.getValsLinear(x))] for x in cons]
+
+    cons_map = sorted(cons_map, key=lambda x: [x[1], str(x[0])])
+    cons = [x[0] for x in cons_map]
+
+    lcons = ncons
+    c_nodes = []
+    for cind, c in enumerate(cons):
+        coeff = m.getValsLinear(c)
+        rhs = m.getRhs(c)
+        lhs = m.getLhs(c)
+        # A[cind][-2]=rhs
+        sense = 0
+
+        if rhs == lhs:
+            sense = 2
+        elif rhs >= 1e+20:
+            sense = 1
+            rhs = lhs
+
+        summation = 0
+        for k in coeff:
+            v_indx = v_map[k]
+            # A[cind][v_indx]=1
+            # A[cind][-1]+=1
+            if coeff[k] != 0:
+                indices_spr[0].append(cind)
+                indices_spr[1].append(v_indx)
+                values_spr.append(1)
+            v_nodes[v_indx][2] += 1
+            v_nodes[v_indx][1] += coeff[k] / lcons
+            v_nodes[v_indx][3] = max(v_nodes[v_indx][3], coeff[k])
+            v_nodes[v_indx][4] = min(v_nodes[v_indx][4], coeff[k])
+            # v_nodes[v_indx][3]+=cind*coeff[k]
+            summation += coeff[k]
+        llc = max(len(coeff), 1)
+        c_nodes.append([summation / llc, llc, rhs, sense])
+    c_nodes.append(obj_node)
+    v_nodes = torch.as_tensor(v_nodes, dtype=torch.float32)#.to(device)
+    c_nodes = torch.as_tensor(c_nodes, dtype=torch.float32)#.to(device)
+    b_vars = torch.as_tensor(b_vars, dtype=torch.int32)#.to(device)
+
+    A = torch.sparse_coo_tensor(indices_spr, values_spr, (ncons + 1, nvars))#.to(device)
+    clip_max = [20000, 1, torch.max(v_nodes, 0)[0][2].item()]
+    clip_min = [0, -1, 0]
+
+    v_nodes[:, 0] = torch.clamp(v_nodes[:, 0], clip_min[0], clip_max[0])
+
+    maxs = torch.max(v_nodes, 0)[0]
+    mins = torch.min(v_nodes, 0)[0]
+    diff = maxs - mins
+    for ks in range(diff.shape[0]):
+        if diff[ks] == 0:
+            diff[ks] = 1
+    v_nodes = v_nodes - mins
+    v_nodes = v_nodes / diff
+    v_nodes = torch.clamp(v_nodes, 1e-5, 1)
+    # v_nodes=position_get_ordered(v_nodes)
+    # v_nodes=position_get_ordered_flt(v_nodes)
+
+    maxs = torch.max(c_nodes, 0)[0]
+    mins = torch.min(c_nodes, 0)[0]
+    diff = maxs - mins
+    c_nodes = c_nodes - mins
+    c_nodes = c_nodes / diff
+    c_nodes = torch.clamp(c_nodes, 1e-5, 1)
+
+    return A, v_map, v_nodes, c_nodes, b_vars
+
+def transform_l_map_to_v_map(l_map:dict):
+    v_map = {key: idx // 2 for key, idx in l_map.items() if idx % 2 == 0}
+    for key, idx in l_map.items():
+        if idx % 2 == 1:
+            assert key[:-len("_complement")] in v_map, f"Variable {key} not found in v_map"
+            assert v_map[key[:-len("_complement")]] == idx // 2, f"Variable {key} not found in v_map with the same index" 
+    return v_map
+def get_standard_bipartite_graph(instance_filepath:str, graph_type:str):
+    if graph_type == VARIABLES_GRAPH:
+        A, v_map, v_nodes, c_nodes, b_vars = get_bipartite_graph_variables(instance_filepath)
+        bipartite_graph = (A, v_map, v_nodes, c_nodes)
+    elif graph_type == LITERALS_GRAPH:
+        A, l_map, l_nodes, c_nodes = get_bipartite_graph_literals(instance_filepath)
+        v_map = transform_l_map_to_v_map(l_map)
+        bipartite_graph = (A, v_map, l_nodes, c_nodes)
+    else:
+        raise ValueError(f"Unknown graph type requested: {graph_type}")
+    return bipartite_graph
